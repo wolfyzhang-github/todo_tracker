028e98e01126cae2a2264e503bf194dfa526c8a0
wolfyzhang <wolfyzhang@outlook.com>
2024-08-20 14:37:34 +0800

    add test

---
+++ test_file.js
@@ -0,0 +1,33 @@
+// 测试JavaScript文件
+
+function testFunction1() {
+    // TODO: 普通优先级任务
+    console.log("测试函数1");
+}
+
+function testFunction2() {
+    // TODO: 低优先级任务!
+    console.log("测试函数2");
+}
+
+function testFunction3() {
+    // TODO: 中优先级任务!!
+    console.log("测试函数3");
+}
+
+function testFunction4() {
+    // TODO: 高优先级任务!!!
+    console.log("测试函数4");
+}
+
+class TestClass {
+    constructor() {
+        // TODO: 需要初始化属性
+    }
+    
+    method() {
+        // TODO: 实现方法逻辑
+    }
+}
+
+// TODO: 添加更多功能 
---
+++ test_file.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 这是一个测试文件，用于测试TODO标记器
+
+def function1():
+    """这是一个示例函数"""
+    # TODO: 这是一个普通优先级的TODO
+    print("这是函数1")
+
+def function2():
+    """这是另一个示例函数"""
+    # TODO: 这是一个低优先级的TODO!
+    print("这是函数2")
+
+def function3():
+    """这是第三个示例函数"""
+    # TODO: 这是一个中优先级的TODO!!
+    print("这是函数3")
+
+def function4():
+    """这是第四个示例函数"""
+    # TODO: 这是一个高优先级的TODO!!!
+    print("这是函数4")
+
+class TestClass:
+    def method1(self):
+        # TODO: 类方法中的TODO
+        pass
+        
+# TODO: 未实现的功能 
